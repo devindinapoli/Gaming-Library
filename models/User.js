3aa9873ecad78e8efd2a5ba928f53f71dd870b40
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-27 14:32:14 -0400

    Updated the User model to hold username and password

--- models/User.js
+++ models/User.js
@@ -13,7 +13,16 @@ const UserSchema = new Schema({
     required: true,
     unique: true
   },
-
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true
+  },
   consoles: {
     type: String,
     required: true,
