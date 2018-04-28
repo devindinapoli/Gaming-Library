5358469585bf6f4a63822f952ba7ef4266d267f6
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-28 15:18:12 -0400

    Fixed models

--- models/User.js
+++ models/User.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // define the schema for our user model
-var userSchema = mongoose.Schema({
+const UserSchema = new Schema({
   local: {
     email: String,
     password: String
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
   }
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
