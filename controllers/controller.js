854c9fa82ca776e0ead4b46509913264f57c7ea1
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-29 10:10:46 -0400

    Functioning POST for submitting user info. Functioning GET for pulling user info from DB

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,50 +16,43 @@ module.exports = function(app, passport) {
   });
 
   app.get("/profile", function(req, res) {
-    res.sendFile(
-      path.join(__dirname, "../public/profile.html", {
-        user: req.user
-      })
-    );
+    res.sendFile(path.join(__dirname, "../public/profile.html"));
   });
 
-  // process the signup form
-  app.post(
-    "/signup",
-    passport.authenticate("local-signup", {
-      successRedirect: "/profile", // redirect to the secure profile section
-      failureRedirect: "/", // redirect back to the signup page if there is an error
-      failureFlash: true // allow flash messages
-    })
-  );
-
-  // =====================================
-  // PROFILE SECTION =========================
-  // =====================================
-  // we will want this protected so you have to be logged in to visit
-  // we will use route middleware to verify this (the isLoggedIn function)
-  app.get("/profile", function(req, res) {
-    res.sendFile(
-      path.join(__dirname, "../public/profile.html", {
-        user: req.user
+  app.post("/submit", function(req, res) {
+    db.User.create(req.body)
+      .then(function(dbUser) {
+        res.json(dbUser);
       })
-    );
+      .catch(function(err) {
+        res.json(err);
+      });
   });
 
-  // =====================================
-  // LOGOUT ==============================
-  // =====================================
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
+  app.post("/submitGame", function(req, res) {
+    db.Game.create(req.body)
+      .then(function(dbGame) {
+        return db.User.findOneAndUpdate(
+          {},
+          { $push: { games: dbGame._id } },
+          { new: true }
+        );
+      })
+      .then(function(dbUser) {
+        res.json(dbUser);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.get("/user", function(req, res) {
+    db.User.find({})
+      .then(function(dbUser) {
+        res.json(dbUser);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
   });
 };
-
-// route middleware to make sure
-function isLoggedIn(req, res, next) {
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated()) return next();
-
-  // if they aren't redirect them to the home page
-  res.redirect("/");
-}
