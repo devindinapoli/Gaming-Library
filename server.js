469c16e5f64cb3c55e33beb029bf2e9bdc045a81
Andrew <jaknowles1997@gmail.com>
2018-04-28 18:59:26 -0700

    server.js set up for passport

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./models");
+var passport = require('passport');
 
-const passport = require("passport");
-require("./config/passport.js")(passport);
+//  require("./config/passport")(passport);
+// Import Routes
+require("./controllers/controller.js")(app, passport);
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,14 +17,15 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 
 // Required for passport
+app.use(session({secret: "keyboard cat"}));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
-require("./controllers/controller.js")(app, passport);
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/VGDB";
 
