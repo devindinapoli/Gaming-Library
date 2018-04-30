77bf5e702535a31f873b6ffeece9aa0f7921ca2a
sergio <sergioburca@gmail.com>
2018-04-30 11:22:25 -0400

    Fixed Jquery CDN link

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,32 +1,35 @@
 $(document).ready(function () {
-  $('.ui.rating').rating();
-  // $('.ui.dropdown').dropdown();
-  // $('.ui.form').form({
-  //   fields: {
-  //     email: {
-  //       identifier: 'email',
-  //       rules: [{
-  //           type: 'empty',
-  //           prompt: 'Please enter your e-mail'
-  //         },
-  //         {
-  //           type: 'email',
-  //           prompt: 'Please enter a valid e-mail'
-  //         }
-  //       ]
-  //     },
-  //     password: {
-  //       identifier: 'password',
-  //       rules: [{
-  //           type: 'empty',
-  //           prompt: 'Please enter your password'
-  //         },
-  //         {
-  //           type: 'length[6]',
-  //           prompt: 'Your password must be at least 6 characters'
-  //         }
-  //       ]
-  //     }
-  //   }
-  // });
+  $('.ui.rating').rating({
+    initialRating: 3,
+    maxRating: 5
+  });
+  $('.ui.dropdown').dropdown();
+  $('.ui.form').form({
+    fields: {
+      email: {
+        identifier: 'email',
+        rules: [{
+            type: 'empty',
+            prompt: 'Please enter your e-mail'
+          },
+          {
+            type: 'email',
+            prompt: 'Please enter a valid e-mail'
+          }
+        ]  
+      },
+      password: {
+        identifier: 'password',
+        rules: [{
+            type: 'empty',
+            prompt: 'Please enter your password'
+          },
+          {
+            type: 'length[6]',
+            prompt: 'Your password must be at least 6 characters'
+          }
+        ]
+      }
+    }
+  });
 });
